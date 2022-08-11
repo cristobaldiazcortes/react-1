c91d535b1613822f7be70fe1c9b8825f95b7fb96
Cristobal Javier Diaz Cortes <cristobaldps4@gmail.com>
2022-08-11 01:05:03 -0400

    segundo commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,18 @@
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 const Footer = (props) => {
-    return (
-      <div>
-        <Container>
-            <div className='bajada'>
-                <p>{props.titulo}</p>   
-                <Button variant="warning" href="https//:www.google.cl" target="_blank">{props.boton}</Button>
-            </div>
-        </Container>
-      </div>
-    );
-}    
+  return (
+    <div>
+      <Container>
+        <div className="bajada">
+          <p className="titulo-bajada">{props.titulo}</p>
+          <Button variant="warning" type="submit">
+            <a href="https://google.com" target="_blank" rel="noreferrer">  {props.boton}  </a>
+          </Button>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 export default Footer;
